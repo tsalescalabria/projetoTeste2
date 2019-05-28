e9917724d93cc3fc904d017b76e7845f0dbe9c44
Thiago Calabria <thiago.calabria@holosmedia.com>
2019-05-28 15:49:03 -0300

    novos

--- app/Http/Controllers/ContatoControlle.php
+++ app/Http/Controllers/ContatoControlle.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contato;
 
 class ContatoControlle extends Controller
 {
     public function index()
     {
-        return "Esse é o Index do ContatoController";
+       
+
+        $contato = new Contato();
+        dd($contato->lista());
     }
 }
